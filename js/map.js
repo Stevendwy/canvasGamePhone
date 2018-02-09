431845e586e46ccf7358d6e38e5bef7123d88aa4
steven <bluedwy@126.com>
2018-02-09 16:47:31 +0800

    games  on phone by canvas
    打飞机 贪吃蛇 贝塞尔曲线

---
+++ js/map.js
@@ -0,0 +1,42 @@
+(function(name,data){
+ if(typeof onTileMapLoaded === 'undefined') {
+  if(typeof TileMaps === 'undefined') TileMaps = {};
+  TileMaps[name] = data;
+ } else {
+  onTileMapLoaded(name,data);
+ }})("map",
+{ "height":10,
+ "layers":[
+        {
+         "data":[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+         "height":10,
+         "name":"\u5757\u5c42 1",
+         "opacity":1,
+         "type":"tilelayer",
+         "visible":true,
+         "width":10,
+         "x":0,
+         "y":0
+        }],
+ "nextobjectid":1,
+ "orientation":"orthogonal",
+ "renderorder":"right-down",
+ "tileheight":50,
+ "tilesets":[
+        {
+         "columns":1,
+         "firstgid":1,
+         "image":"map.jpg",
+         "imageheight":50,
+         "imagewidth":50,
+         "margin":0,
+         "name":"map",
+         "spacing":0,
+         "tilecount":1,
+         "tileheight":50,
+         "tilewidth":50
+        }],
+ "tilewidth":50,
+ "version":1,
+ "width":10
+});
